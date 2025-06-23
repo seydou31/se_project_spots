603dbaaaeaaa468b55d50a4329b5d6d61aa21e80
Seydou <badiakaseydou@gmail.com>
2025-06-23 14:20:29 -0400

    SE-SPOTS FINAL STAGE

--- script/index.js
+++ script/index.js
@@ -4,9 +4,19 @@ const EditButton = document.querySelector(".profile__edit");
 const profileCloseBtn = profileEditModal.querySelector(".modal__close-button");
 const postBtn = document.querySelector(".profile__button");
 const postCloseBtn = profilePostModal.querySelector(".modal__close-button");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const inputName = profileEditModal.querySelector("#input-name");
+const inputDescription = profileEditModal.querySelector("#input-description");
+const formSubmit = profileEditModal.querySelector(".modal__form");
+const addCardFormElement = profilePostModal.querySelector(".modal__form");
+const linkInput = addCardFormElement.querySelector("#card-image-name");
+const captionInput = addCardFormElement.querySelector("#card-caption-name");
 
 EditButton.addEventListener("click", function () {
   profileEditModal.classList.add("modal_is-opened");
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
 });
 
 profileCloseBtn.addEventListener("click", function () {
@@ -18,5 +28,19 @@ postBtn.addEventListener("click", function () {
 });
 
 postCloseBtn.addEventListener("click", function () {
+  profilePostModal.classList.remove("modal_is-opened");
+});
+
+formSubmit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  profileEditModal.classList.remove("modal_is-opened");
+});
+
+addCardFormElement.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(linkInput.value);
+  console.log(captionInput.value);
   profilePostModal.classList.remove("modal_is-opened");
 });
