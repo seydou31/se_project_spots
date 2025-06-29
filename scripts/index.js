d1fddb86e219d8fb62de44f6d2b31a1ff9bb182b
Seydou <badiakaseydou@gmail.com>
2025-06-29 04:57:58 -0400

    SE-SPOTS Improve code readability F

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 const profileEditModal = document.querySelector("#edit-profile-modal");
-const profilePostModal = document.querySelector("#new-post-modal");
+const newPostModal = document.querySelector("#new-post-modal");
 const EditButton = document.querySelector(".profile__edit");
 const profileCloseBtn = profileEditModal.querySelector(".modal__close-button");
 const postBtn = document.querySelector(".profile__button");
-const postCloseBtn = profilePostModal.querySelector(".modal__close-button");
+const postCloseBtn = newPostModal.querySelector(".modal__close-button");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const inputName = profileEditModal.querySelector("#input-name");
 const inputDescription = profileEditModal.querySelector("#input-description");
 const formSubmit = profileEditModal.querySelector(".modal__form");
-const addCardFormElement = profilePostModal.querySelector(".modal__form");
+const addCardFormElement = newPostModal.querySelector(".modal__form");
 const linkInput = addCardFormElement.querySelector("#card-image-name");
 const captionInput = addCardFormElement.querySelector("#card-caption-name");
 
@@ -34,18 +34,18 @@ profileCloseBtn.addEventListener("click", function () {
 });
 
 postBtn.addEventListener("click", function () {
-  openModal(profilePostModal);
+  openModal(newPostModal);
 });
 
 postCloseBtn.addEventListener("click", function () {
-  closeModal(profilePostModal);
+  closeModal(newPostModal);
 });
 
 function handleProfileFormSubmit(e) {
   e.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
-  closeModal(profilePostModal);
+  closeModal(profileEditModal);
 }
 formSubmit.addEventListener("submit", handleProfileFormSubmit);
 
@@ -53,7 +53,7 @@ function handleCardFormSubmit(e) {
   e.preventDefault();
   console.log(linkInput.value);
   console.log(captionInput.value);
-  closeModal(profilePostModal);
+  closeModal(newPostModal);
   addCardFormElement.reset();
 }
 addCardFormElement.addEventListener("submit", handleCardFormSubmit);
